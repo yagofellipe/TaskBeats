c3b4be06f0a11a3439463da8187b739808c80f21
yagofellipe <yagofellipe09@gmail.com>
2023-12-16 12:00:14 -0300

    recycle

--- app/src/main/java/com/comunidadedevspace/taskbeats/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/MainActivity.kt
@@ -2,10 +2,18 @@ package com.comunidadedevspace.taskbeats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val taskList: RecyclerView = findViewById(R.id.rv_task_list)
+
+        val list = listOf<String>("Titlesvsgbweegb 1","Agatha","Title 3")
+        val adapter = TaskListAdapter(list)
+
+         taskList.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskListAdapter.kt
@@ -0,0 +1,42 @@
+package com.comunidadedevspace.taskbeats
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class TaskListAdapter(private val titles: List<String>): RecyclerView.Adapter<TaskListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListViewHolder {
+
+        val view: View = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_task, parent, false)
+
+        return  TaskListViewHolder(view)
+    }
+
+    // Tamanho da minha lista
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+
+    override fun onBindViewHolder(holder: TaskListViewHolder, position: Int) {
+        val item = titles[position]
+        holder.bind(item)
+    }
+}
+
+class TaskListViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    val tvTaskTitle = view.findViewById<TextView>(R.id.tv_task_title)
+    val tvTaskDescription = view.findViewById<TextView>(R.id.tv_task_description)
+
+    fun bind(title: String){
+        tvTaskTitle.text = title
+        tvTaskDescription.text = title
+    }
+}
+
