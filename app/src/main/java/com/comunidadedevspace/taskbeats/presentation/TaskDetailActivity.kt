3f752eeedb9ddc9a719a6120108270f02c75c93c
yagofellipe <yagofellipe09@gmail.com>
2023-12-29 20:02:34 -0300

    Layers-UI-DATA

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskDetailActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskDetailActivity.kt
@@ -1,4 +1,4 @@
-package com.comunidadedevspace.taskbeats
+package com.comunidadedevspace.taskbeats.presentation
 
 import android.app.Activity
 import android.content.Context
@@ -11,6 +11,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.comunidadedevspace.taskbeats.R
+import com.comunidadedevspace.taskbeats.data.Task
 import com.google.android.material.snackbar.Snackbar
 
 class TaskDetailActivity : AppCompatActivity() {
@@ -22,7 +24,7 @@ class TaskDetailActivity : AppCompatActivity() {
         fun start(context: Context, task: Task?): Intent{
             val intent = Intent(context, TaskDetailActivity::class.java)
                 .apply {
-                    putExtra(TaskDetailActivity.TASK_DETAIL_EXTRA, task)
+                    putExtra(TASK_DETAIL_EXTRA, task)
                 }
             return intent
         }
--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
@@ -1,4 +1,4 @@
-package com.comunidadedevspace.taskbeats
+package com.comunidadedevspace.taskbeats.presentation
 
 import android.app.Activity
 import android.os.Bundle
@@ -9,6 +9,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.comunidadedevspace.taskbeats.R
+import com.comunidadedevspace.taskbeats.data.AppDatabase
+import com.comunidadedevspace.taskbeats.data.Task
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
